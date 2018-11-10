0c46b5b0d3419d55ff261c13de4857e7abe9bd38
jjh2v2 <jjh2v2@naver.com>
2018-11-11 03:04:32 +0900

    좀만 더하면 된다

--- Assets/Assets/Circle.cs
+++ Assets/Assets/Circle.cs
@@ -13,15 +13,16 @@ public class Circle : MonoBehaviour {
     public GameObject HitObj;
     public GameObject HitBg;
     public GameObject CountText;
-    public float speed = 1.0f;
-    public float addspeed = 0.1f;
+    public GameObject NewBestScoreText;
+    public float speed = 2.0f;
 
     public Color ChaingeColor;
     private Color OriginClor;
 
     private bool mTouch = false;
     private ulong mHitCount = 0;
-
+    private ulong mStageCurrentCount = 0;
+    private int mBestScore = 0;
     private Vector3 PreRot;
 
     void Awake()
@@ -33,16 +34,15 @@ public class Circle : MonoBehaviour {
     // Use this for initialization
     void Start ()
     {
-        //HitObj = this.GetComponentInChildren(GameObject)
-        Image HitBgImg = HitBg.GetComponent<Image>();
-        OriginClor = HitBgImg.color;
-        CountText.GetComponent<Text>().text = "0";
+        ////HitObj = this.GetComponentInChildren(GameObject)
+        //Image HitBgImg = HitBg.GetComponent<Image>();
+        //OriginClor = HitBgImg.color;
+        //CountText.GetComponent<Text>().text = "0";
 
-        patical.GetComponent<ParticleSystem>().Stop();
+        //patical.GetComponent<ParticleSystem>().Stop();
         //patical.GetComponent<ParticleSystem>().playbackSpeed = 1.30f;
 
-        float ran = Random.Range(90.0f, 270.0f);
-        Rotation(0.0f, ran);
+        Reset();
 
         //GameObject _game = new GameObject();
         //_game.AddComponent<RectTransform>();
@@ -64,69 +64,56 @@ public class Circle : MonoBehaviour {
 	void Update () {
         if (HitBg && HitObj)
         {
-            //z += Time.deltaTime * 50;
-            //HitObj.transform.rotation = Quaternion.Euler(0, 0, z);
+            ////z += Time.deltaTime * 50;
+            ////HitObj.transform.rotation = Quaternion.Euler(0, 0, z);
             HitObj.transform.localRotation *= Quaternion.AngleAxis(speed, new Vector3(0.0f, 0.0f, 1.0f));
-
-            Image HitBgImg = HitBg.GetComponent<Image>();
-            Image HitObjImg = HitObj.GetComponent<Image>();
-
-            //float dddd = HitObj.transform.eulerAngles.z;
-            //Debug.Log("HitObj.transform.eulerAngles.z : " + dddd);
 
             float oZ = (PreRot.z);
             float fZ = (HitObj.transform.localEulerAngles.z);
-            //Debug.Log("HitObj.transform.localRotation.z : " + fZ);
             if (Mathf.Abs(fZ - oZ) < 0.1f)
             {
                 GameOver();
             }
 
-            float HitBgImgOverPos = HitBg.transform.eulerAngles.z - (HitBgImg.fillAmount * 360);
-            float HitObjImgOverPos = HitObj.transform.eulerAngles.z - (HitObjImg.fillAmount * 360);
-            if (HitBg.transform.eulerAngles.z > HitObj.transform.eulerAngles.z)
+            Image HitBgImg = HitBg.GetComponent<Image>();
+            Image HitObjImg = HitObj.GetComponent<Image>();
+
+            float HitBgImgPosOut = HitBg.transform.eulerAngles.z - (HitBgImg.fillAmount * 360);
+            float HitBgImgPosIn = HitBg.transform.eulerAngles.z;
+            float HitObjImgPosOut = HitObj.transform.eulerAngles.z;// + (HitObjImg.fillAmount * 360);
+            float HitObjImgPosIn = HitObj.transform.eulerAngles.z - (HitObjImg.fillAmount * 360);
+
+
+            bool isHit = false;
+            if (HitBgImgPosOut < 0.0f)
             {
-                if (HitBgImgOverPos < HitObjImgOverPos)
+                //// 0도에 걸쳐있을때
+                float HitBgImgPosOutTemp = 360.0f + HitBgImgPosOut;
+                if (360.0f >= HitObjImgPosIn && HitBgImgPosOutTemp <= HitObjImgPosOut)
                 {
                     // 충돌
-                    HitBgImg.color = ChaingeColor;
-                    if (mTouch)
-                    {
-                        Opposition();
-                    }
-                }
-                else
-                {
-                    HitBgImg.color = OriginClor;
-                    if (mTouch)
-                    {
-                        GameOver();
-                    }
+                    isHit = true;
                 }
             }
-            else if (HitBgImgOverPos < HitObjImgOverPos)
+            if (HitBgImgPosIn >= HitObjImgPosIn && HitBgImgPosOut <= HitObjImgPosOut)
             {
-                float _eulerAngles = HitBg.transform.eulerAngles.z;
-                float _fillAmount = (HitBg.GetComponent<Image>().fillAmount * 360);
-                float dsfs = _fillAmount - _eulerAngles;
-                if (360.0f - dsfs > HitObj.transform.eulerAngles.z)
+                // 충돌
+                isHit = true;
+            }
+            if (isHit)
+            {
+                HitBgImg.color = ChaingeColor;
+                if (true/*mTouch*/)
                 {
-                    // 충돌
-                    HitBgImg.color = ChaingeColor;
-                    if (mTouch)
-                    {
-                        Opposition();
-                    }
+                    Opposition();
                 }
-                else
+            }
+            else
+            {
+                HitBgImg.color = OriginClor;
+                if (mTouch)
                 {
-                    HitBgImg.color = OriginClor;
-                        Debug.Log("1111111 : " + dsfs);
-                    if (mTouch)
-                    {
-                        GameOver();
-                    }
-                        Debug.Log("1111111 : ");
+                    GameOver();
                 }
             }
         }
@@ -138,6 +125,59 @@ public class Circle : MonoBehaviour {
         // 충돌 카운트 처리
         mHitCount++;
         CountText.GetComponent<Text>().text = "" + mHitCount;
+
+        // 충돌 배경 위치 선정
+        float ran = Random.Range(90.0f, 270.0f);
+        HitBg.transform.localRotation *= Quaternion.AngleAxis(ran, new Vector3(0.0f, 0.0f, 1.0f));
+
+        // 충돌하고 바운드 설정을 위해 데이터 가져와서 가공하기
+        uint _hitCurrent = 0;
+        for (uint i = 0; i < mStageCurrentCount + 1; i++)
+        {
+            _hitCurrent += GameDataBase.StageHitCount[i];
+        }
+        if (mHitCount == _hitCurrent && mStageCurrentCount < GameDataBase.StagetotalCount)
+        {
+            mStageCurrentCount++;
+        }
+       if (mStageCurrentCount >= GameDataBase.StagetotalCount)
+       {
+            mStageCurrentCount = 19;
+       }
+
+        // 충돌 바운드 설정
+        Image HitBgImg = HitBg.GetComponent<Image>();
+        float RangeMin = GameDataBase.RangeMinMaxData[mStageCurrentCount, 0];
+        float RangeMax = GameDataBase.RangeMinMaxData[mStageCurrentCount, 1];
+        float yyyy = Random.Range(RangeMin, RangeMax);
+        HitBgImg.fillAmount = yyyy;
+        //Debug.Log("yyyy : " + yyyy);
+
+        // 스피드 설정
+        int iii = Random.Range(0, 1);
+        float _Speed = GameDataBase.SpeedMinMaxData[mStageCurrentCount, iii];
+        if (speed > 0.0f)
+        {
+            speed += _Speed;
+        }
+        else
+        {
+            speed -= _Speed;
+        }
+        if (mStageCurrentCount == GameDataBase.StagetotalCount - 1)
+        {
+            if (speed < 0.0f)
+            {
+                _Speed = 5.5f;
+            }
+            else
+            {
+                _Speed = -5.5f;
+            }
+        }
+        speed *= -1.0f;
+
+        Debug.Log("speed : " + speed);
 
         // 충돌하지 않고 한바퀴 돌았을때 게임종료를 위해서 이전 충돌 위치 저장
         PreRot = HitObj.transform.localEulerAngles;
@@ -154,37 +194,70 @@ public class Circle : MonoBehaviour {
         patical.GetComponent<ParticleSystem>().Stop();
         patical.GetComponent<ParticleSystem>().Play();
 
-        // 충돌 배경 위치 선정
-        float ran = Random.Range(90.0f, 270.0f);
-        HitBg.transform.localRotation *= Quaternion.AngleAxis(ran, new Vector3(0.0f, 0.0f, 1.0f));
+        PlayerPrefs.SetInt("bestScore", (int)mHitCount);
+        PlayerPrefs.Save();
+        //if (PlayerPrefs.HasKey("bestScore"))
+        //{
+        //    // 해당 키가 있으면 mBestScore
+        //    PlayerPrefs.GetInt("bestScore", mBestScore);
+        //}
+        PlayerPrefs.GetInt("bestScore", mBestScore);
+        NewBestScoreText.GetComponent<Text>().text = "" + mBestScore;
 
-        // 충돌객체와 충돌배경 난이도 처리
-        // 횟수가 중요한거니까 횟수가 올라갈수록 속도를 빠르게
-        speed *= -1.0f;
-        if (mHitCount%2 == 0)
-        {
-            if(speed > 0)
-                speed += addspeed;
-        }
-        if (mHitCount % 2 == 0)
-        {
-            Image HitBgImg = HitBg.GetComponent<Image>();
-            if (HitBgImg.fillAmount > 0.012f)
-            {
-                HitBgImg.fillAmount -= 0.01f;
-            }
-        }
+        mTouch = false;
     }
 
     // 0 ~ 360 -값은 들어가면 중돌 계산이 안된다
-    private void Rotation(float PlayerR, float BackGroundR)
+    private void Reset()
     {
         if (HitBg && HitObj)
         {
+            mBestScore = 0;
+            if (PlayerPrefs.HasKey("bestScore"))
+            {
+                // 해당 키가 있으면 mBestScore
+                PlayerPrefs.GetInt("bestScore", mBestScore);
+            }
+            else
+            {
+                PlayerPrefs.SetInt("bestScore",0);
+                PlayerPrefs.Save();
+            }
+
+            NewBestScoreText.GetComponent<Text>().text = "" + mBestScore;
+
+            Image HitBgImg = HitBg.GetComponent<Image>();
+            OriginClor = HitBgImg.color;
+            CountText.GetComponent<Text>().text = "0";
+
+            patical.GetComponent<ParticleSystem>().Stop();
+
+            float PlayerR = 0.0f;
+            float BackGroundR = Random.Range(90.0f, 270.0f);
+
             HitObj.transform.localRotation *= Quaternion.AngleAxis(PlayerR, new Vector3(0.0f, 0.0f, 1.0f));
             HitBg.transform.localRotation *= Quaternion.AngleAxis(BackGroundR, new Vector3(0.0f, 0.0f, 1.0f));
 
             PreRot = HitObj.transform.localEulerAngles;
+
+            mStageCurrentCount = 0;
+            mHitCount = 0;
+
+            float RangeMin = GameDataBase.RangeMinMaxData[mStageCurrentCount, 0];
+            float RangeMax = GameDataBase.RangeMinMaxData[mStageCurrentCount, 1];
+            float yyyy = Random.Range(RangeMin, RangeMax);
+            HitBgImg.fillAmount = yyyy;
+
+            int iii = Random.Range(0, 1);
+            float _Speed = GameDataBase.SpeedMinMaxData[mStageCurrentCount, iii];
+            if (mStageCurrentCount == GameDataBase.StagetotalCount - 1)
+            {
+                _Speed = Mathf.Abs(6.0f - speed);
+            }
+            speed += _Speed;
+            speed *= -1.0f;
+
+            mTouch = false;
         }
     }
 
---
+++ Assets/Assets/GameDataBase.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameDataBase
+{
+
+    public const int StagetotalCount = 20;
+    public static uint[] StageHitCount = new uint[StagetotalCount]
+    {
+        3,
+        3,
+        4,
+        4,
+        5,
+        5,
+        6,
+        6,
+        7,
+        7,//10
+        8,
+        8,
+        9,
+        9,
+        10,
+        10,
+        11,
+        11,
+        12,
+        12//20
+    };// 150히트
+
+    public static float[,] RangeMinMaxData = new float[StagetotalCount,2]
+    {
+        {0.25f, 0.25f},
+        {0.25f, 0.25f},
+        {0.24f, 0.25f},
+        {0.24f, 0.25f},
+        {0.23f, 0.25f},
+        {0.22f, 0.25f},
+        {0.22f, 0.25f},
+        {0.19f, 0.22f},
+        {0.19f, 0.22f},
+        {0.16f, 0.19f},//10
+        {0.16f, 0.19f},
+        {0.13f, 0.16f},
+        {0.13f, 0.16f},
+        {0.10f, 0.13f},
+        {0.10f, 0.13f},
+        {0.07f, 0.10f},
+        {0.07f, 0.10f},
+        {0.05f, 0.07f},
+        {0.05f, 0.07f},
+        {0.05f, 0.25f}//20
+    };
+
+    public static float[,] SpeedMinMaxData = new float[StagetotalCount, 2]
+    {
+        {0.0f, 0.05f},
+        {0.1f, 0.2f},
+        {0.0f, 0.05f},
+        {0.05f, 0.2f},
+        {0.1f, 0.2f},
+        {0.05f, 0.1f},
+        {0.0f, 0.2f},
+        {0.0f, 0.1f},
+        {0.1f, 0.2f},
+        {0.05f, 0.1f},//10
+        {0.1f, 0.2f},
+        {0.0f, 0.2f},
+        {0.05f, 0.1f},
+        {0.0f, 0.1f},
+        {0.0f, 0.1f},
+        {0.0f, 0.1f},
+        {0.0f, 0.1f},
+        {0.0f, 0.1f},
+        {0.0f, 0.1f},
+        {0.0f, 0.0f}//20
+    };
+
+}
